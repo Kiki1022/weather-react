448040c2fd8f7e727e8df77178b8d3715c1c3f23
kinnari <kinnari1022@yahoo.com>
2021-07-04 17:29:44 -0400

    add image folder and css styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+import React from 'react'
 
+const API = {
+  key: "bca53e1784fdcb9782e4f44bc75ca9a6", 
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <main>
+        <div className="search-box">
+          <input 
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          />
+        </div> 
+     </main>
     </div>
   );
 }
